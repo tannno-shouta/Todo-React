816767c3213359f28a1216f64c22ddc5c3ad1998
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2022-10-05 17:33:11 +0900

    initial commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,27 +1,57 @@
-
 import {Header} from "./components/Header";
 import {InputToDo} from "./components/InputToDo";
+import {Filter} from "./components/Filter";
 import {ToDo} from "./components/ToDo";
 import { useState } from "react";
 
-export const App =() => {
-  const [todo, setToDo] = useState([
-    {
-      done:true,
-      text:'text1'
-    },
-    {
-      done:true,
-      text:'text2'
-    }
-  ])
-  return(
+export const ToDoApp = () => {
+ 
+  // ランダムなキーを取得
+  const getKey = () => Math.random().toString(32).substring(2);
+ 
+  // stateを作成
+  const [todos, setToDos] = useState([]);
+  const [filter, setFilter] = useState('ALL');
+
+  // 入力値をtodos(配列)に設定
+  const handleAdd = text => {
+    setToDos([...todos, { key: getKey(), text, done: false }]);
+  };
+
+  // フィルターの切り替え
+  const handleFilterChange = value => setFilter(value);
+ 
+  // フィルターに応じたToDoを表示
+  const displayToDos = todos.filter(todo => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !todo.done;
+    if (filter === 'DONE') return todo.done;
+  });
+
+
+  // チェックボックス判定
+  const handleCheck = checked => {
+    // チェックがついたToDoの真偽値(done)を変更
+    const newToDos = todos.map(todo => {
+      if (todo.key === checked.key) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    });
+    setToDos(newToDos);
+  };
+
+  return (
     < >
       <Header></Header>
-      <InputToDo></InputToDo>
-      <ToDo todo={todo}></ToDo>
+      <InputToDo onAdd={handleAdd}></InputToDo>
+      <Filter onChange={handleFilterChange}
+              value={filter}></Filter>
+      <ToDo key={todo.key}
+            todo={todo}
+            onCheck={handleCheck}></ToDo>
     </>
   );
 };
 
-export default App;
+export default ToDoApp;
