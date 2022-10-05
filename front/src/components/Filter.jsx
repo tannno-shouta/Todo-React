1fb79a3add04c812af1d3f1f362c954f2534f1aa
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2022-10-05 18:27:55 +0900

    Filter componentsにStyle jsxを追加

--- front/src/components/Filter.jsx
+++ front/src/components/Filter.jsx
@@ -12,24 +12,37 @@ export const Filter = (props) => {
   };
  
   return (
-    <div className="panel-tabs">
-      <a
-        href="#"
-        onClick={handleClick.bind(null, 'ALL')}
-        className={classNames({ 'is-active': value === 'ALL' })}
-      >All</a>
-      <a
-        href="#"
-        onClick={handleClick.bind(null, 'TODO')}
-        className={classNames({ 'is-active': value === 'TODO' })}
-      >ToDo</a>
-      <a
-        href="#"
-        onClick={handleClick.bind(null, 'DONE')}
-        className={classNames({ 'is-active': value === 'DONE' })}
-      >Done</a>
-    </div>
+    <>
+      <div className="panel-block">
+        <a
+          href="#"
+          onClick={handleClick.bind(null, 'ALL')}
+          className={classNames({ 'is-active': value === 'ALL' })}
+        >All</a>
+        <a
+          href="#"
+          onClick={handleClick.bind(null, 'TODO')}
+          className={classNames({ 'is-active': value === 'TODO' })}
+        >ToDo</a>
+        <a
+          href="#"
+          onClick={handleClick.bind(null, 'DONE')}
+          className={classNames({ 'is-active': value === 'DONE' })}
+        >Done</a>
+      </div>
+
+      <style jsx> {`
+          .panel-block {
+            width: 100%; 
+            padding: 30px 50px;
+            font-size: 20px;
+            border-radius: 3px; 
+            border: 2px solid #ddd; 
+            box-sizing: border-box; 
+          }
+      `}</style>
+    </>
   );
-}
+};
  
 export default Filter;
