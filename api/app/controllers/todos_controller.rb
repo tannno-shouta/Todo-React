864051ac5829030824cc022cf74557a7fd1f2b29
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2022-11-02 14:53:57 +0900

    ‘追加機能’

--- api/app/controllers/todos_controller.rb
+++ api/app/controllers/todos_controller.rb
@@ -8,4 +8,17 @@ class TodosController < ApplicationController
   def new 
     @todos = Todo.new
   end
+
+  def create
+    @todos = Todo.new
+  if  @todos.save
+      redirect_to root_path
+  else
+    render :new
+  end
+
+  def destroy
+    @todos.destroy
+    redirect_to root_path
+  end
 end
