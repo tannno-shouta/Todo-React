619f52515d02f7eb6c0e0dc20398700b8dfe58a2
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2022-11-08 17:43:39 +0900

    ‘add_delete’

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: "todos#index"
   post '/' , to:"todos#create"
-  delete '/' , to:"todos#delete"
+  delete '/' , to:"todos#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
